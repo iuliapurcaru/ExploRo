0afe7e5db53b786c915967b03e31c8738c899df1
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2024-04-03 14:26:55 +0300

    Added LoginActivity binding

--- app/src/main/java/com/example/exploro/login/LoginActivity.java
+++ app/src/main/java/com/example/exploro/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.exploro.login;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.exploro.R;
+import com.example.exploro.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
