8b7c5c669e9139e448db212d4062b9bc559c0574
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2024-04-03 14:15:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/exploro/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.exploro.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.exploro.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
