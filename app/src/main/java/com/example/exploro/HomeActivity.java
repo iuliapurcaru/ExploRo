4ad36e73e4b9c789aab49493a12de5711f234b4e
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2024-04-14 22:17:59 +0300

    Added Logout functionality

--- app/src/main/java/com/example/exploro/HomeActivity.java
+++ app/src/main/java/com/example/exploro/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.example.exploro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import com.example.exploro.databinding.FragmentAccountBinding;
+import com.example.exploro.ui.account.AccountFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -9,14 +14,17 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.exploro.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        com.example.exploro.databinding.ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(findViewById(R.id.toolbar));
@@ -30,8 +38,6 @@ public class HomeActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        final ProgressBar progressBar = findViewById(R.id.loading);
-
+        mAuth = FirebaseAuth.getInstance();
     }
-
 }
--- app/src/main/java/com/example/exploro/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/exploro/ui/account/AccountFragment.java
@@ -1,17 +1,23 @@
 package com.example.exploro.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import com.example.exploro.HomeActivity;
+import com.example.exploro.LoginActivity;
 import com.example.exploro.databinding.FragmentAccountBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountFragment extends Fragment {
 
+    FirebaseAuth mAuth;
     private FragmentAccountBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -22,8 +28,21 @@ public class AccountFragment extends Fragment {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        mAuth = FirebaseAuth.getInstance();
+
         final TextView textView = binding.textAccount;
+        final Button logoutButton = binding.logout;
+        final View progressBar = binding.loading;
+
         accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        logoutButton.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            mAuth.signOut();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        });
+
         return root;
     }
 
